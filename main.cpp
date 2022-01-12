1828d92c44fd0bc7f99422cfd57e5545eca8970d
Andrzej Borkowski <borkowski.andrzej@protonmail.com>
2022-01-12 09:40:14 +0100

    komentarze

--- main.cpp
+++ main.cpp
@@ -17,16 +17,21 @@ int main()
     std::vector< int > rand_nums;
     unsigned _int64       sevens = 0;
 
+    //wektor liczb losowych
     rand_nums = make_random_vector(20, 0, 10);
+    //print
     for_each(rand_nums.begin(), rand_nums.end(), [](int& i) { std::cout << i << " "; });
     std::cout << "\n";
 
+    //sortowanie rosnaco
     std::sort(rand_nums.begin(), rand_nums.end());
     for_each(rand_nums.begin(), rand_nums.end(), [](int& i) { std::cout << i << " "; });
 
+    //zliczanie
     sevens = std::count(rand_nums.begin(), rand_nums.end(), 7);
     std::cout << "\nsevens: " << sevens << "\n";
 
+    //modyfikacja sortowania roznymi sposobami
     //funkcja
     //std::sort(rand_nums.begin(), rand_nums.end(), funk);
     //funktor (struktura)
@@ -35,10 +40,12 @@ int main()
     //std::sort(rand_nums.begin(), rand_nums.end(), [](int a, int b) { return a > b; });
     for_each(rand_nums.begin(), rand_nums.end(), [](int& i) { std::cout << i << " "; });
 
+    //zliczanie warunkowe
     int above_sevens =
         std::count_if(rand_nums.begin(), rand_nums.end(), [](int& i) { return i > 7; });
     std::cout << "\nabove 7: "<< above_sevens << "\n";
 
+    //zliczanie warunkowe z inputem (lambda)
     int a = 0;
     std::cin >> a;
     int above_input = std::count_if(rand_nums.begin(), rand_nums.end(), [a](int& i){
