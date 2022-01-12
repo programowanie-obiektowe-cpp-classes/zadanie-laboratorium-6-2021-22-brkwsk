cf5a7dd930da67fd2f39e6242f796961679e7806
Andrzej Borkowski <borkowski.andrzej@protonmail.com>
2022-01-12 09:09:49 +0100

    funkcja, funktor, lambda

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 #include "make_random_vector.hpp"
 
+int funk(const int& a, const int& b)
+{
+    return a > b;
+};
+
+struct F
+{
+    int operator()(const int& a, const int& b) { return a > b; };
+};
 
 int main()
 {
@@ -18,7 +27,12 @@ int main()
     sevens = std::count(rand_nums.begin(), rand_nums.end(), 7);
     std::cout << "\nsevens: " << sevens << "\n";
 
-    std::sort(rand_nums.begin(), rand_nums.end(), [](int a, int b) { return a > b; });
+    //funkcja
+    //std::sort(rand_nums.begin(), rand_nums.end(), funk);
+    //funktor (struktura)
+    std::sort(rand_nums.begin(), rand_nums.end(), F());
+    //lambda
+    //std::sort(rand_nums.begin(), rand_nums.end(), [](int a, int b) { return a > b; });
     for_each(rand_nums.begin(), rand_nums.end(), [](int& i) { std::cout << i << " "; });
 
 }
